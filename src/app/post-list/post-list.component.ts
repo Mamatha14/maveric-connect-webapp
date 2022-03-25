ebcda424c773b90a6754b8194fe3b13d11a26b7e
Mamatha E <mamathae@maveric-systems.com>
2022-03-25 09:12:19 +0530

    Post List feature included.

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  postArray: any =[];
+  constructor(private http:HttpClient) {
+    let url="http://localhost:8000/posts";
+    this.http.get(url).subscribe((response:any)=>{
+      console.log(response)
+      this.postArray=response;
+    })
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
